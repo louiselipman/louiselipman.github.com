40f88ee3154c36ac584f141538b3a6a39b9313ac
Isobel Redelmeier <iredelmeier@gmail.com>
2013-03-20 00:32:44 -0400

    Testing PHP

--- upload.php
+++ upload.php
@@ -1,7 +1,8 @@
-<?php
-$imgName = $_POST["image"];
-$imgAlt = $_POST["alt"];
-$myFile = "test.json";
-$fileWrite = fopen($myFile, "w") or die("can't open file");
-$fileArray = json_decode($myFile);
-echo $fileArray;
+<html>
+<body>
+
+<?php echo $_POST["image"]; ?></br>
+<?php echo $_POST["alt"]; ?></br>
+
+</body>
+</html>
